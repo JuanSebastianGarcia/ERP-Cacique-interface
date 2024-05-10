5620294f9b2d6e868e64134394e166cd9d2e93bc
Juan Sebastian Garcia Alzate <117672609+JuanSebastianGarcia@users.noreply.github.com>
2024-05-10 11:26:08 -0500

    se creo la estructura de carpetas basada en una estructura comunmente usada, ademas se esta construyendo el enrutamiento de cada componente, esta pendiente realizar la direccion al componente empleado y producto

--- my-app/src/app/app.routes.ts
+++ my-app/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path:'', 
+        loadChildren: () => import('./page/login/login.routes').then( m => m.LOGIN_ROUTES)
+    }
+];
---
+++ my-app/src/app/page/login/login.routes.ts
@@ -0,0 +1,8 @@
+import { Routes } from "@angular/router";
+import { IngresarComponent } from "./ingresar//ingresar.component";
+
+export const LOGIN_ROUTES: Routes=[
+
+    {path:'',component: IngresarComponent}
+
+];
