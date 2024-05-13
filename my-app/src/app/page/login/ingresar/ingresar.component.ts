44cda9bcf617d36ea752eb61b390ef3e2cc9abd2
Juan Sebastian Garcia Alzate <117672609+JuanSebastianGarcia@users.noreply.github.com>
2024-05-13 00:10:02 -0500

    se esta desarrollando la funcionalidad que permite el ingreso(login) a la plataforma, ya se capturan los datos, esta pendiente crear el servicio y realizar la conexion

--- my-app/src/app/page/login/ingresar/ingresar.component.ts
+++ my-app/src/app/page/login/ingresar/ingresar.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { LoginDto } from '../../../core/models/login-dto';
 
 @Component({
   selector: 'app-ingresar',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './ingresar.component.html',
   styleUrl: './ingresar.component.css'
 })
 export class IngresarComponent {
 
+
+
+  loginData: LoginDto  = {
+    email: '',
+    password:''  
+  }
+
+  constructor(){};
+
+  
+  login() {
+    
+    console.log(this.loginData)
+
+  }
 }
