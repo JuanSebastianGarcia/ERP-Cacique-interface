1208e946a9f641b90ac65dc31d7b64615cd10196
Juan Sebastian Garcia Alzate <117672609+JuanSebastianGarcia@users.noreply.github.com>
2024-06-01 22:11:05 -0500

    se esta configurando el nuevo componente para la gestion de los datos de configuracion de cada producto

---
+++ my-app/src/app/page/producto/configuration-types/configuration-types.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-configuration-types',
+  standalone: true,
+  imports: [],
+  templateUrl: './configuration-types.component.html',
+  styleUrl: './configuration-types.component.css'
+})
+export class ConfigurationTypesComponent {
+
+}
--- my-app/src/app/page/producto/producto.routes.ts
+++ my-app/src/app/page/producto/producto.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from "@angular/router";
 import { VisualizarProductosComponent } from "./visualizar-productos/visualizar-productos.component";
 import { CrearProductoComponent } from "./crear-producto/crear-producto.component";
 import { EditarProductoComponent } from "./editar-producto/editar-producto.component";
+import { ConfigurationTypesComponent } from "./configuration-types/configuration-types.component";
 
 
 export const PRODUCTO_ROUTES:Routes = [
 
     {path:'',component: VisualizarProductosComponent},
     {path:'crear-producto',component:CrearProductoComponent},
-    {path:'editar-producto',component:EditarProductoComponent}
+    {path:'editar-producto',component:EditarProductoComponent},
+    {path:'editar-tipo-Configuracion',component:ConfigurationTypesComponent}
 ];
