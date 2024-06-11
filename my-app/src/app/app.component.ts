cd126b388ce45780db7781606e263f9091e7c141
Juan Sebastian Garcia Alzate <117672609+JuanSebastianGarcia@users.noreply.github.com>
2024-06-11 10:45:44 -0500

    se corrigio un bug en el que los componentes se cargaban por debajo del tool-bar y no dentro de el

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -8,27 +8,27 @@ import { RouterModule } from '@angular/router';
 import {MatCardModule} from '@angular/material/card';
 import {MatTableModule} from '@angular/material/table';
 import {ProductoService } from './core/service/producto.service';
+import { IngresarComponent } from "./page/login/ingresar/ingresar.component";
 
 
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [
-            RouterOutlet, 
-            BarraLateralComponent,
-            CommonModule,
-            RouterModule,
-            MatCardModule,
-            MatTableModule,
-
-
-  ],
-  providers: [LoginService,
-              TokenService,
-              ProductoService],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    providers: [LoginService,
+        TokenService,
+        ProductoService],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [
+        RouterOutlet,
+        BarraLateralComponent,
+        CommonModule,
+        RouterModule,
+        MatCardModule,
+        MatTableModule,
+        IngresarComponent
+    ]
 })
 export class AppComponent {
   title = 'my-app';
